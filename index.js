36a7189081f27a7e7cf09aafca0096370ecf7d9d
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2021-06-19 19:43:05 +0300

    Готово

--- index.js
+++ index.js
@@ -1,30 +1,57 @@
 const refs = {
-  
-}
+  daysEl: document.querySelector('[data-value="days"]'),
+  hoursEl: document.querySelector('[data-value="hours"]'),
+  minsEl: document.querySelector('[data-value="mins"]'),
+  secsEl: document.querySelector('[data-value="secs"]'),
+};
 
 class CountdownTimer {
-  selector;
-  targetDate;
-
-  constructor({ selector, targetDate }) {
-    this.selector = selector;
-    this.targetDate = targetDate;
+  constructor({ сlockFace, beforeTravelDate }) {
+    this.сlockFace = сlockFace,
+      this.beforeTravelDate = beforeTravelDate,
+      this.onInterfaceStart()
   }
-}
+
+  onInterfaceStart() {
+    const timeComponents = this.getTimeElements(0);
+    this.сlockFace(timeComponents);
+  }
+
+  start() {
+
+    setInterval(() => {
+      
+      const currentTime = Date.now();
+      const timeLeft = this.beforeTravelDate - currentTime;
+      const timeComponents = this.getTimeElements(timeLeft);
+      
+      this.сlockFace(timeComponents);
   
-  const newTimer = new CountdownTimer({
-    selector: '#timer-1',
-    targetDate: new Date('Sep 25, 2021'),
+    }, 1000);      
+  }
+
+  getTimeElements(time) {
+  const days = this.addZero(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const mins = this.addZero(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = this.addZero(Math.floor((time % (1000 * 60)) / 1000));
+  const hours = this.addZero(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+
+  return {days, hours, mins, secs};
+  }
+  
+  addZero = (num) => num < 10 ? '0' + num : num ;
+};
+  
+const newTimer = new CountdownTimer({    
+    сlockFace: getClockFace,
+    beforeTravelDate: new Date('Sep 25, 2022'),
 });
 
+function getClockFace({ days, hours, mins, secs }) {
+  refs.daysEl.textContent = days;
+  refs.hoursEl.textContent = hours;
+  refs.minsEl.textContent = mins;
+  refs.secsEl.textContent = secs;
+};
 
-console.log(newTimer.targetDate);
-
-// getTimeElements(time) {
-//   const days = Math.floor(time / (1000 * 60 * 60 * 24));
-//   const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-//   const secs = Math.floor((time % (1000 * 60)) / 1000);
-
-//   return { days, hours, mins, secs };
-// };
+newTimer.start();
